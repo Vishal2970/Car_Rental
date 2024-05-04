f6ef65e78a9532f43a9ebbeeaeec05a4b1c663b8
vishal2970 <vishalagraharibasti@gmail.com>
2024-05-04 16:51:06 +0530

    added some Classes

--- Car_Rental/Main.java
+++ Car_Rental/Main.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
     class Car{
         private String Car_Id;
@@ -14,16 +18,16 @@ public class Main {
             this.isAvailable=true;
 
         }
-        public String GetCar_Id(){
+        public String getCar_Id(){
             return Car_Id;
         }
-        public String GetBrand(){
+        public String getBrand(){
             return Brand;
         }
-        public String GetModel(){
+        public String getModel(){
             return Model;
         }
-        public double GetBase_Price_perday(int Number_of_Day){
+        public double getBase_Price_perday(int Number_of_Day){
             return Number_of_Day*Base_Price_perday;
         }
         public boolean isAvailable(){
@@ -44,6 +48,100 @@ public class Main {
         public Customer(String Customer_Id,String Customer_Name){
             this.Customer_Name=Customer_Name;
             this.Customer_Id=Customer_Id;
+        }
+        public String getCustomer_Id(){
+            return Customer_Id;
+        }
+        public String getCustomer_Name(){
+            return Customer_Name;
+        }
+    }
+
+    class Rental{
+        private Car car;
+        private Customer customer;
+        private int days;
+        public Rental(Car car, Customer customer,int days){
+            this.car=car;
+            this.customer=customer;
+            this.days=days;
+        }
+        public Car getCar(){
+            return car;
+        }
+        public Customer getCustomer(){
+            return customer;
+        }
+        public int getDays(){
+            return days;
+        }
+    }
+
+    class CarRentalSysytem{
+        private List<Car> cars;
+        private List<Customer> customers;
+        private List<Rental>rentals;
+
+        public CarRentalSysytem(){
+            cars=new ArrayList<>();
+            customers=new ArrayList<>();
+            rentals=new ArrayList<>();
+        }
+
+        public void addCar(Car car){
+            cars.add(car);
+        }
+        public void addCustomer(Customer customer){
+            customers.add(customer);
+        }
+
+        public void rentACar(Car car, Customer customer, int days){
+            if (car.isAvailable){
+                car.Rent();
+                rentals.add(new Rental(car,customer,days));
+            }else {
+                System.out.println("Car is not avaliable for rent");
+            }
+        }
+        public void returnCar(Car car){
+            car.Return_Car();
+            Rental rentalToRemove=null;
+            for(Rental rental:rentals){
+                if (rental.getCar() == car){
+                    rentalToRemove=rental;
+                    break;
+                }
+            }
+            if (rentalToRemove !=null){
+                rentals.remove(rentalToRemove);
+                System.out.println("Car Return successfully");
+            }else {
+                System.out.println("Car was not returned");
+            }
+        }
+    }
+
+    public void menu(){
+        Scanner sc =new Scanner(System.in);
+        while (true){
+            System.out.println("===============Car Rental System===============");
+            System.out.println("1. Rent a car");
+            System.out.println("2. Exit");
+            System.out.println("Enter your choice");
+            int choice=sc.nextInt();
+            sc.nextLine();
+
+            if (choice == 1){
+                System.out.println("\n==Rent a car==\n");
+                System.out.println("Enter Your Name");
+                String Customer_Name=sc.next();
+                System.out.println("\n Available cars");
+                for(Car car: Car){
+                    if (car.isAvailable){
+                        System.out.println(car.getCar_Id()+" - "+car.getBrand()+" - "+ car.getModel());
+                    }
+                }
+            }
         }
     }
 
